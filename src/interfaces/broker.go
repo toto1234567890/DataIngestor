0c96aa509fc61266c71f58da6b3c67e4a06bdb77
toto1234567890 <34986356+toto1234567890@users.noreply.github.com>
2025-12-07 12:35:48 +0100

    Create broker.go

---
+++ src/interfaces/broker.go
@@ -0,0 +1,41 @@
+package interfaces
+
+import (
+	"trading-system/src/config"
+	"trading-system/src/logger"
+	"trading-system/src/models"
+)
+
+// -----------------------------------------------------------------------------
+
+// BrokerConstructor defines the function signature for creating a new IBroker instance.
+type IBrokerConstructor func(config *config.Config, logger *logger.Logger, name string) (IBroker, error)
+
+// -----------------------------------------------------------------------------
+
+// Broker defines the core interface for all exchange broker implementations
+type IBroker interface {
+	// GetName return the broker name
+	GetName() string
+
+	// GetType return the asset type (equity, crypto, stock...)
+	GetType() string
+
+	// GetEndPoint return the API endpoint of the broker (for display/logging)
+	GetEndPoint() string
+
+	// GetConnectionEndpoint returns the full endpoint with credentials for WebSocket connection
+	GetEndpointWithCredentials() string
+
+	// GetSymbols return the subscriber symbols list
+	GetSymbols() []string
+
+	// AddSubscription creates subscription message for symbols
+	AddSubscription(symbols []string) ([]byte, error)
+
+	// RemoveSubscription removes subscription for symbols
+	RemoveSubscription(symbols []string) ([]byte, error)
+
+	// ParseMessage processes incoming messages into MarketData
+	ParseMessage(message []byte) (*models.MMarketData, error)
+}
